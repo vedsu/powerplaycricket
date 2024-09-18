becbe2f3f7c6bb44fb4796665219237dc6aab076
vedsu <90701239+vedsu@users.noreply.github.com>
2024-09-18 17:32:59 +0530

    Update sponsers.py

--- info/sponsers.py
+++ info/sponsers.py
@@ -62,7 +62,7 @@ for donor in donor_list:
     with col3:
         st.markdown(f"""
             <div style='text-align: center;'>
-                <img src="{donor['Photo']}" style="border-radius: 50%; width:120px; height:120px; margin-top:20px; box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;">
+                <img src="{donor['Photo']}" style="border-radius: 50%; width:120px; height:120px; margin-top:10px; box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;">
             </div>
         """, unsafe_allow_html=True)
     st.markdown(f"""
